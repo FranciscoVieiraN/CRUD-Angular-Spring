cc3a8459b9393c8b269fd5cd6d2af89aeab8ca09
Francisco Vieira <vieirafrancisco122@gmail.com>
2022-06-21 09:42:55 -0300

    Update crud-angular Aula 21

---
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Course } from './../model/course';
+import { delay, first, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private readonly API = 'api/courses';
+
+  constructor(private httpCliente: HttpClient) { }
+
+  list() {
+    return this.httpCliente.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      //delay(4000),
+      tap( courses => console.log(courses))
+    );
+  }
+
+  save(record: Course) {
+    return this.httpCliente.post<Course>(this.API, record).pipe(first());
+  }
+
+}
